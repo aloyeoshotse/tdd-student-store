7d0e8530821d39bf521073f9e1b3b4ec410db9ec
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-06-21 13:42:31 -0700

    added routes

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -3,12 +3,19 @@ import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import "./App.css"
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, Routes} from "react-router-dom"
 
 export default function App() {
   return (
     <div className="app">
       <BrowserRouter>
+        {/* adding these routes removes the views from the page. if error continues, comment it out */}
+        <Routes>
+          <Route path="Home" element={<Home />}/>
+          <Route path="About" element={<About />}/>
+          <Route path="Contact" element={<Contact />}/>
+          <Route path="Buy" element={<Buy />} />
+        </Routes>
         <main>
           {/* YOUR CODE HERE! */}
           <Navbar />
