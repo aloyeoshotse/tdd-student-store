846c1589127020f0c60a8b74f3cc8257b0efb7d1
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-06-21 15:33:42 -0700

    Changed the style of the sidebar

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -4,7 +4,10 @@ import "./Sidebar.css"
 export default function Sidebar() {
   return (
     <section className="sidebar">
-      <p>Sidebar</p>
+      <button id="open">open arrow</button>
+      <button id="cart">shopping cart</button>
+      <button id="payment">payment info</button>
+      <button id="checkout">checkout info</button>
     </section>
   )
 }
