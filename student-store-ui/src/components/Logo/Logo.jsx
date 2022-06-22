ba1e77cd0d98bfbbc41389197cfc4ba321488895
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-06-21 22:01:51 -0700

    added Logo component

---
+++ student-store-ui/src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import "./Logo.css"
+import { NavLink } from "react-router-dom"
+
+export function Logo(props) {
+    <div className="logo">
+        <NavLink to={"/"} />
+    </div>
+}
+
+export default Logo
